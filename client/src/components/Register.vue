6e92bcc6fd551bfc874d5e81c66f854d3766672e
StellarCrow <dariadar12@gmail.com>
2019-04-21 19:46:07 +0300

    bootstrap-vue and scss setup

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,35 +1,46 @@
 <template>
-    <div>
-      <h1>Register</h1>
-      <input type="text" name="email" placeholder="email" v-model="email">
-      <input type="text" name="password" placeholder="password" v-model="password">
-      <br/>
-      <button @click="register">Register</button>
-
-    </div>
+  <div>
+    <h1>Register</h1>
+    <b-container>
+      <b-row>
+        <b-col>
+          <input type="text" name="email" placeholder="email" v-model="email">
+        </b-col>
+        <b-col>
+          <input type="text" name="password" placeholder="password" v-model="password">
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <button @click="register">Register</button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
   import AuthenticationService from '../services/AuthenticationService'
-    export default {
-        name: "Register",
-      data() {
-        return {
-          email: '',
-          password: ''
-        }
-      },
-      methods: {
-          async register() {
-            await AuthenticationService.register({
-              email: this.email,
-              password: this.password
-            });
-          }
+
+  export default {
+    name: "Register",
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async register() {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
       }
     }
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "../styles/components/register";
 </style>
